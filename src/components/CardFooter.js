92cfcd1c30bec68d7b0924023ddf666b632c53b9
chalobon <chalobon-jaje@hotmail.com>
2021-10-09 14:00:31 +0700

    add footer component

---
+++ src/components/CardFooter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CardFooter = () => {
+  return (
+    <div className="card-footer">
+      <h2>Get early access today</h2>
+      <p>
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+      <div className="input-btn">
+        <input type="text" name="" id="" />
+        <button>Get Started For Free</button>
+      </div>
+    </div>
+  );
+};
+
+export default CardFooter;
